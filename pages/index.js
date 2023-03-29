18fccb256162f943fbda8ffef88e4a290098c790
usfalidiate <51782274+usfalidiate@users.noreply.github.com>
2023-03-29 22:05:54 +1100

    v2c

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import MainPage from './MainPage';
 import NewMainPage from './NewMainPage';
 
 export default function Home() {
@@ -15,7 +14,6 @@ return (
   </Head>
 
 
-  {/* <MainPage /> */}
   <NewMainPage/>
 
 </>
